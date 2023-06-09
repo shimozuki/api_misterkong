82fb43aafc4c61dfffb1b69a81c7c80e0bdc79df
shimozuki <robbishimozuki@gmail.com>
2023-06-09 16:35:23 +0800

    master

--- app/Http/Controllers/API/RiderController.php
+++ app/Http/Controllers/API/RiderController.php
@@ -12,15 +12,13 @@ class RiderController extends Controller
     {
         $resi = $request->resi;
         $query = DB::table('t_penjualan_detail')->join('m_barang_satuan', 't_penjualan_detail.item_id', '=', 'm_barang_satuan.barang_id')
-        ->join('m_barang', 'm_barang.id', '=', 'm_barang_satuan.barang_id')->where('t_penjualan_detail.no_transaksi', $resi)->select('m_barang.nama','t_penjualan_detail.qty','t_penjualan_detail.harga_jual')->first();
+        ->join('m_barang', 'm_barang.id', '=', 'm_barang_satuan.barang_id')->where('t_penjualan_detail.no_transaksi', $resi)->select('m_barang.nama','t_penjualan_detail.qty','t_penjualan_detail.harga_jual', '')->get();
 
         if (!empty($query)) {
             return response()->json([
                 'status' => 1,
                 'message' => 'Data ditemuka',
-                'menu' => $query->nama,
-                'haraga' => $query->harga_jual,
-                'qty' => $query->qty
+                'menu' => $query
             ]);
         }else{
             return response()->json([
